c8a0e95859587ed9917b128612043ba2fc6f4bd0
Favian David Martinez Martin (Externo) (Colombia) <ext_fmartinez@mallplaza.com>
2025-03-22 21:37:03 -0500

    se guardan cambios en Home.css

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -7,29 +7,33 @@ const Home = () => {
 
 
   return (
-    <div className="fondoHome">
+    <div >
 
       <video autoPlay loop muted className="videoFondo">
         <source src={fondo} type="video/mp4" />
         Tu navegador no soporta videos.
       </video>
+      
+      <div className="fondoHome">
+        <div className="presentacionContainer">
+          <img className="imgFoto" src={fPresentacion} alt="Imagen de presentación" />
+          <div className="presentacion">
 
-      <div className="presentacionContainer">
-        <img className="imgFoto" src={fPresentacion} alt="Imagen de presentación" />
-        <div className="presentacion">
+            <h2 className="nombre">Favian David Martínez</h2>
+            <p>Programador especializado en frontend. Me apasiona diseñar software para la gestión y automatización de procesos. Disfruto aprender constantemente y cuento con conocimientos en HTML, CSS y JavaScript, utilizando bibliotecas como React. Además, tengo experiencia en trabajo en equipo con Git y GitHub, formación en Platzi y estudios en MySQL y PHP en el SENA</p>
 
-          <h2 className="nombre">Favian David Martínez</h2>
-          <p>Programador especializado en frontend. Me apasiona diseñar software para la gestión y automatización de procesos. Disfruto aprender constantemente y cuento con conocimientos en HTML, CSS y JavaScript, utilizando bibliotecas como React. Además, tengo experiencia en trabajo en equipo con Git y GitHub, formación en Platzi y estudios en MySQL y PHP en el SENA</p>
+          </div>
 
         </div>
 
+        <div className="contenedorHVida">
+          <a href={hVida} download={hVida}>
+            <button>Descargar <br /> Hoja de vida</button>
+          </a>
+        </div>
       </div>
 
-      <div className="contenedorHVida">
-        <a href={hVida} download={hVida}>
-          <button>Descargar <br /> Hoja de vida</button>
-        </a>
-      </div>
+      
       
     </div>
       
